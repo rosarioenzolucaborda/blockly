7e29bc26f16dfab187dfc8a6ee1a9523a6cdf343
ellen.spertus <ellen.spertus@gmail.com>
2013-11-26 03:15:02 -0800

    Automatic commit Tue Nov 26 03:15:02 PST 2013

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 git rm -r apps blocks core demos generators i18n media msg tests
 rm -rf apps blocks core demos generators i18n media msg tests
 cp -r ~/src-mirror/blockly/{apps,blocks,core,demos,generators,i18n,media,msg,tests} .
+git add -A
 git commit -m "Automatic commit `date`"
 git push
